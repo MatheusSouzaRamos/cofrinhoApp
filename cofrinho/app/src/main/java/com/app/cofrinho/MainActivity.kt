c4326d9387b954dd36781c9ecf5334e6123b20c4
MatheusSouzaRamos <144972414+MatheusSouzaRamos@users.noreply.github.com>
2026-07-26 18:08:52 -0300

    iniciando projeto
    Botão de criar novo indo para tela de cadastro

---
+++ cofrinho/app/src/main/java/com/app/cofrinho/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.app.cofrinho
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.app.cofrinho.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        binding.btnNew.setOnClickListener {
+            irParaTelaCadastro()
+        }
+    }
+
+    private fun irParaTelaCadastro(){
+        val intent = Intent(this, MainActivity2::class.java)
+        startActivity(intent)
+    }
+}
