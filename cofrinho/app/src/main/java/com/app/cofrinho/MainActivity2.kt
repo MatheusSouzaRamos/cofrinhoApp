0af47bee1abcbf8dfe5556af77b198b85bd2237d
MatheusSouzaRamos <144972414+MatheusSouzaRamos@users.noreply.github.com>
2026-07-27 17:24:40 -0300

    botao de retorna para a tela inicial

--- cofrinho/app/src/main/java/com/app/cofrinho/MainActivity2.kt
+++ cofrinho/app/src/main/java/com/app/cofrinho/MainActivity2.kt
@@ -1,20 +1,30 @@
 package com.app.cofrinho
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.app.cofrinho.databinding.ActivityMain2Binding
+import com.app.cofrinho.databinding.ActivityMainBinding
 
 class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main2)
+
+        val binding = ActivityMain2Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        binding.btnRtn.setOnClickListener {
+            finish()
         }
     }
 }
