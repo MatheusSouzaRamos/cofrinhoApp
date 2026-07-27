8a8b53a6a16c4acf84eab2ffbc1f9c072b5c6709
MatheusSouzaRamos <144972414+MatheusSouzaRamos@users.noreply.github.com>
2026-07-27 17:42:33 -0300

    iniciando função de criar

--- cofrinho/app/src/main/java/com/app/cofrinho/MainActivity2.kt
+++ cofrinho/app/src/main/java/com/app/cofrinho/MainActivity2.kt
@@ -2,6 +2,7 @@ package com.app.cofrinho
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -22,6 +23,26 @@ class MainActivity2 : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+
+        binding.btnCriar.setOnClickListener {
+            val nome = binding.nomeCriar.text.toString().trim()
+            val metaStr = binding.metaCriar.text.toString().trim()
+            val meta = metaStr.toDoubleOrNull()
+            val imagem = 1 // IMPLEMENTAR
+
+            if(!nome.isEmpty() && meta != null && imagem != null) {
+                Toast.makeText(this,
+                    "Nome: $nome, Meta: $meta, Imagem: $imagem",
+                    Toast.LENGTH_SHORT).show();
+            }else{
+                Toast.makeText(this,
+                    "Preencha todos os campos!",
+                    Toast.LENGTH_SHORT).show();
+            }
+        }
+
+
 
         binding.btnRtn.setOnClickListener {
             finish()
